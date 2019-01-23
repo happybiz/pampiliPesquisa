6c9d2ea6002b40224364667f5907489d71093d27
liliane06 <liliane@happybiz.com.br>
2019-01-23 11:24:46 -0200

    Pesquisa dos loks

--- public/js/colecao.js
+++ public/js/colecao.js
@@ -1,5 +1,5 @@
 (function ($){
-    $('.colecao-wrraper-flex').slick({
+    var slider = $('.colecao-wrraper-flex').slick({
         centerMode: true,
         //centerPadding: '60px',
         slidesToShow: 3,
@@ -32,15 +32,19 @@
     $('.slick-prev, .slick-next').on('click',function(){ 
         $('.slick-slide button').attr('disabled', true);      
         $('.slick-current button').attr('disabled', false);
+        $('.slick-current .botoes-colecao button').removeClass('btn-cinza');
     });
 
     $('.botoes-colecao button').on('click', function(){
         $('.slick-current .botoes-colecao button').removeClass('borda-check');
-        $('.slick-current .botoes-colecao button').removeClass('btn-cinza, btn-ativo');
-        $('.slick-current .botoes-colecao button').toggleClass('btn-cinza');
+        $('.slick-current .botoes-colecao button').addClass('btn-cinza');
+        $('.slick-current .botoes-colecao button').removeClass('btn-ativo');
+        
         $(this).toggleClass('borda-check'); 
         $(this).parents('.bg-colecao').addClass('img-certo');
-        $(this).toggleClass('btn-ativo');
+        $(this).addClass('btn-ativo');
+        slider.slick('slickNext');
+        $('.slick-current button').attr('disabled', false);
 
         var count = $('.img-certo').length;
         var universal = $('.slick-slide').length;
